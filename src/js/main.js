daceda39c467c9f980db0c4f11fa7a2838293bf2
elisha111 <elisey.kovalyov@yandex.ru>
2025-03-25 23:33:17 +0300

    добавил css дебаг

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,41 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// import './style.css'
+<script>
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+			// Рекламный баннер B1_M (850*250)
+			window.Ya.adfoxCode.create({
+				ownerId: 295846,
+				containerId: 'adfox_160328950210922240',
+				params: {
+					pp: 'g',
+					ps: 'deex',
+					p2: 'gzep'
+				}
+			});
+			console.log('Рекламный баннер B1_M (850*250)');
+		} else {
+			// Рекламный баннер B1_D (850*110)
+			window.Ya.adfoxCode.create({
+				ownerId: 295846,
+				containerId: 'adfox_160319557762354659',
+				params: {
+					pp: 'g',
+					ps: 'deex',
+					p2: 'gzea'
+				}
+			});
+			console.log('Рекламный баннер B1_D (850*110)');
+			document.addEventListener('DOMContentLoaded', function(){
+				setTimeout(function() {
+					var topBanner = $("#adfox_160319557762354659")[0].innerHTML;
+                    //console.log($("#adfox_160319557762354659"), topBanner, 1);
+					if(topBanner != ''){
+						$("#adfox_160319557762354659").css('display', 'block');
+                        $("#adfox_160319557762354659").css('height', 'unset');
+						$(".lr_slogan").css('display', 'none');
+						$(".lr_slogan-wrap").css('height', 'auto');
+						//console.log(topBanner);
+					}
+				}, 1000);
+			});
+		}
+	</script>
